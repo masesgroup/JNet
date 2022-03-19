5743baef32ad03ec00856b83902d9617c3954fa9
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-03-19 08:26:37 +0100

    Moved classes from JCOBridge

---
+++ src/net/JNet/Java/Lang/CharSequence.cs
@@ -0,0 +1,51 @@
+﻿/*
+*  Copyright 2022 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+using MASES.JCOBridge.C2JBridge;
+
+namespace Java.Lang
+{
+    /// <summary>
+    /// .NET implementations of <see href="https://docs.oracle.com/javase/8/docs/api/java/lang/CharSequence.html"/>
+    /// </summary>
+    public class CharSequence : JVMBridgeBase<CharSequence>
+    {
+        /// <inheritdoc cref="JVMBridgeBase.ClassName"/>
+        public override string ClassName => "java.lang.CharSequence";
+        /// <inheritdoc cref="JVMBridgeBase.IsInterface"/>
+        public override bool IsInterface => true;
+        /// <summary>
+        /// Returns the char value at the specified index. An index ranges from zero to length() - 1. The first char value of the sequence is at index zero, the next at index one, and so on, as for array indexing. 
+        /// </summary>
+        /// <param name="index">The index of the char value to be returned</param>
+        /// <returns>The specified char value</returns>
+        public char CharAt(int index) => IExecute<char>("charAt", index);
+        /// <summary>
+        /// Returns the length of this character sequence.
+        /// </summary>
+        /// <returns>The number of chars in this sequence</returns>
+        public int Length => IExecute<int>("length");
+        /// <summary>
+        /// Returns a CharSequence that is a subsequence of this sequence. The subsequence starts with the char value at the specified index and ends with the char value at index end - 1. The length (in chars) of the returned sequence is end - start, so if start == end then an empty sequence is returned.
+        /// </summary>
+        /// <param name="start">The start index, inclusive</param>
+        /// <param name="end">The end index, exclusive</param>
+        /// <returns>The specified subsequence</returns>
+        public CharSequence SubSequence(int start, int end) => IExecute<CharSequence>("subSequence", start, end);
+    }
+}
