11f1e0774ba335b27b2127630ee5cf41e2846e22
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-04-26 20:13:36 +0200

    Updates on Runtime class

--- src/net/JNet/Java/Lang/Runtime.cs
+++ src/net/JNet/Java/Lang/Runtime.cs
@@ -16,6 +16,7 @@
 *  Refer to LICENSE for more information.
 */
 
+using Java.Io;
 using MASES.JCOBridge.C2JBridge;
 
 namespace Java.Lang
@@ -27,5 +28,91 @@ namespace Java.Lang
     {
         /// <inheritdoc cref="JVMBridgeBase.ClassName"/>
         public override string ClassName => "java.lang.Runtime";
+        /// <summary>
+        /// Registers a new virtual-machine shutdown hook.
+        /// </summary>
+        public void AddShutdownHook​(Thread hook) => IExecute("addShutdownHook​", hook);
+        /// <summary>
+        /// Returns the number of processors available to the Java virtual machine.
+        /// </summary>
+        public int AvailableProcessors => IExecute<int>("availableProcessors​");
+        /// <summary>
+        /// Executes the specified string command in a separate process.
+        /// </summary>
+        public Process Exec​(String command) => IExecute<Process>("exec​​", command);
+        /// <summary>
+        /// Executes the specified command and arguments in a separate process.
+        /// </summary>
+        public Process Exec​(String[] cmdarray) => IExecute<Process>("exec​​", cmdarray);
+        /// <summary>
+        /// Executes the specified command and arguments in a separate process with the specified environment.
+        /// </summary>
+        public Process Exec​(String[] cmdarray, String[] envp) => IExecute<Process>("exec​​", cmdarray, envp);
+        /// <summary>
+        /// Executes the specified command and arguments in a separate process with the specified environment and working directory.
+        /// </summary>
+        public Process Exec​(String[] cmdarray, String[] envp, File dir) => IExecute<Process>("exec​​", cmdarray, envp, dir);
+        /// <summary>
+        /// Executes the specified string command in a separate process with the specified environment.
+        /// </summary>
+        public Process Exec​(String command, String[] envp) => IExecute<Process>("exec​​", command, envp);
+        /// <summary>
+        /// Executes the specified string command in a separate process with the specified environment and working directory.
+        /// </summary>
+        public Process Exec​(String command, String[] envp, File dir) => IExecute<Process>("exec​​", command, envp, dir);
+        /// <summary>
+        /// Terminates the currently running Java virtual machine by initiating its shutdown sequence.
+        /// </summary>
+        public void Exit​(int status) => IExecute("exit​​", status);
+        /// <summary>
+        /// Returns the amount of free memory in the Java Virtual Machine.
+        /// </summary>
+        public long FreeMemory => IExecute<long>("freeMemory​");
+        /// <summary>
+        /// Runs the garbage collector.
+        /// </summary>
+        public void Gc() => IExecute("gc​");
+        /// <summary>
+        /// Returns the runtime object associated with the current Java application.
+        /// </summary>
+        public static Runtime Current => SExecute<Runtime>("getRuntime​");
+        /// <summary>
+        /// Forcibly terminates the currently running Java virtual machine.
+        /// </summary>
+        public void Halt​(int status) => IExecute("halt​​​", status);
+        /// <summary>
+        /// Loads the native library specified by the filename argument.
+        /// </summary>
+        public void Load​(String filename) => IExecute("load​​​​", filename);
+        /// <summary>
+        /// Loads the native library specified by the libname argument.
+        /// </summary>
+        public void LoadLibrary​(String libname) => IExecute("loadLibrary​​​​​", libname);
+        /// <summary>
+        /// Returns the maximum amount of memory that the Java virtual machine will attempt to use.
+        /// </summary>
+        public long MaxMemory => IExecute<long>("freeMemory​");
+        /// <summary>
+        /// De-registers a previously-registered virtual-machine shutdown hook.
+        /// </summary>
+        public bool RemoveShutdownHook​(Thread hook) => IExecute<bool>("removeShutdownHook​", hook);
+        /// <summary>
+        /// Runs the finalization methods of any objects pending finalization.
+        /// </summary>
+        public void RunFinalization() => IExecute("runFinalization​");
+        /// <summary>
+        /// Returns the total amount of memory in the Java virtual machine.
+        /// </summary>
+        public long TotalMemory => IExecute<long>("totalMemory​");
+        /// <summary>
+        /// Returns the version of the Java Runtime Environment as a Runtime.Version.
+        /// </summary>
+        public static Runtime.Version CurrentVersion => SExecute<Runtime.Version>("version");
+
+        public sealed class Version : JVMBridgeBase<Version>
+        {
+            /// <inheritdoc cref="JVMBridgeBase.ClassName"/>
+            public override string ClassName => "java.lang.Runtime.Version";
+        }
     }
 }
