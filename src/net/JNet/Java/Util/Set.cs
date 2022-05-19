ea12631357c9b8b11e896ba5f0fe1dc8f0cb5147
MASES Public Developers Team <94312179+masesdevelopers@users.noreply.github.com>
2022-05-19 20:34:56 +0200

    Update to .NET 462 (#24)
    * Added extra methods
    
    * Version upgrade
    
    * #21: update to .NET 462
    
    * Added and updates on some classes

--- src/net/JNet/Java/Util/Set.cs
+++ src/net/JNet/Java/Util/Set.cs
@@ -21,5 +21,31 @@ namespace Java.Util
     public class Set<E> : Collection<E>
     {
         public override string ClassName => "java.util.Set";
+
+        public static Set<E> CopyOf​<ExtendsE>(Collection<ExtendsE> coll) where ExtendsE : E => SExecute<Set<E>>("copyOf​", coll);
+
+        public static Set<E> Of() => SExecute<Set<E>>("of");
+
+        public static Set<E> Of(E e1) => SExecute<Set<E>>("of", e1);
+
+        public static Set<E> Of(params E[] elements) => SExecute<Set<E>>("of", elements);
+
+        public static Set<E> Of(E e1, E e2) => SExecute<Set<E>>("of", e1, e2);
+
+        public static Set<E> Of(E e1, E e2, E e3) => SExecute<Set<E>>("of", e1, e2, e3);
+
+        public static Set<E> Of(E e1, E e2, E e3, E e4) => SExecute<Set<E>>("of", e1, e2, e3, e4);
+
+        public static Set<E> Of(E e1, E e2, E e3, E e4, E e5) => SExecute<Set<E>>("of", e1, e2, e3, e4, e5);
+
+        public static Set<E> Of(E e1, E e2, E e3, E e4, E e5, E e6) => SExecute<Set<E>>("of", e1, e2, e3, e4, e5, e6);
+
+        public static Set<E> Of(E e1, E e2, E e3, E e4, E e5, E e6, E e7) => SExecute<Set<E>>("of", e1, e2, e3, e4, e5, e6, e7);
+
+        public static Set<E> Of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8) => SExecute<Set<E>>("of", e1, e2, e3, e4, e5, e6, e7, e8);
+
+        public static Set<E> Of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9) => SExecute<Set<E>>("of", e1, e2, e3, e4, e5, e6, e7, e8, e9);
+
+        public static Set<E> Of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10) => SExecute<Set<E>>("of", e1, e2, e3, e4, e5, e6, e7, e8, e9, e10);
     }
 }
