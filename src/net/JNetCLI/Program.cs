1b371e09cfe51db256b4d40a8234aa8f0067f1d8
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2022-08-19 20:50:52 +0200

    Added empty project

---
+++ src/net/JNetCLI/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+*  Copyright 2022 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+using MASES.JCOBridge.C2JBridge;
+using System;
+using System.Reflection;
+using System.Text;
+
+namespace MASES.JNetCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0) { showHelp(); return; }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void showHelp(string errorString = null)
+        {
+            var assembly = typeof(Program).Assembly;
+
+            Console.WriteLine("JNetCLI - JNet command line interface - Version " + assembly.GetName().Version.ToString());
+            Console.WriteLine(assembly.GetName().Name + " <JCOBridgeArguments> <ClassArguments>");
+            Console.WriteLine();
+            if (!string.IsNullOrEmpty(errorString))
+            {
+                Console.WriteLine("Error: {0}", errorString);
+            }
+
+            Console.WriteLine("JCOBridgeArguments: the arguments of JCOBridge (see online at https://www.jcobridge.com/net-examples/command-line-options/ for the possible values). ");
+            Console.WriteLine();
+            Console.WriteLine("Examples:");
+            Console.WriteLine(assembly.GetName().Name + " -ClassToRun ConsoleConsumer --bootstrap-server SERVER-ADDRESS:9093 --topic topic_name --from-beginning");
+        }
+    }
+}
