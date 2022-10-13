7af06819d5bb8e7c5a2892cd139697ec3fbb2a02
MASES Public Developers Team <94312179+masesdevelopers@users.noreply.github.com>
2022-10-13 23:40:24 +0200

    Create docker pipeline (#40)
    * #39: added docker containers

---
+++ src/net/JNetCLI/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+*  Copyright 2022 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+using MASES.JCOBridge.C2JBridge;
+using System;
+using System.Reflection;
+using System.Text;
+
+namespace MASES.JNetCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var assembly = typeof(Program).Assembly;
+                Console.WriteLine($"{assembly.GetName().Name} (ver. {assembly.GetName().Version}) - JNet command line interface");
+                Console.WriteLine();
+                Console.WriteLine("Basic interface to execute something from container.");
+            }
+            catch (Exception e)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(e.Message);
+                Exception innerException = e.InnerException;
+                while (innerException != null)
+                {
+                    sb.AppendLine(innerException.Message);
+                    innerException = innerException.InnerException;
+                }
+                ShowHelp(sb.ToString());
+            }
+        }
+
+        static void ShowHelp(string errorString = null)
+        {
+            var assembly = typeof(Program).Assembly;
+
+            Console.WriteLine($"{assembly.GetName().Name} (ver. {assembly.GetName().Version}) - JNet command line interface");
+            Console.WriteLine($"{assembly.GetName().Name} <JCOBridgeArguments> <ClassArguments>");
+            Console.WriteLine();
+            if (!string.IsNullOrEmpty(errorString))
+            {
+                Console.WriteLine("Error: {0}", errorString);
+            }
+
+            Console.WriteLine("JCOBridgeArguments: the arguments of JCOBridge (see online at https://www.jcobridge.com/net-examples/command-line-options/ for the possible values). ");
+            Console.WriteLine();
+            Console.WriteLine("Examples:");
+            Console.WriteLine(assembly.GetName().Name + " ");
+        }
+    }
+}
