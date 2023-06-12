e016b7518ec74ecae29fb4e8c8f204ac28409bd5
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2023-06-12 14:59:20 +0200

    https://github.com/masesgroup/JNet/issues/178#issue-1728781787: Added namespace to IJNetBridgeMain

--- src/net/JNetReflector/InternalConst.cs
+++ src/net/JNetReflector/InternalConst.cs
@@ -86,7 +86,7 @@ namespace MASES.JNetReflector
         public const string PropertySuffix = "Property";
         public const string MethodSuffix = "Method";
         public const string NamespaceSuffix = "Ns";
-        public const string MainClassPlaceHolder = ", IJNetBridgeMain";
+        public const string MainClassPlaceHolder = ", MASES.JNet.Specific.IJNetBridgeMain";
         public const string JVMBridgeException = "MASES.JCOBridge.C2JBridge.JVMBridgeException";
 
         public static bool IsJavaLangException(string canonicalName)
