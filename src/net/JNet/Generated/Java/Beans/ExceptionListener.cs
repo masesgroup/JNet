d729036e10519e9688aaf01131d8a21d08153cbe
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-07-07 14:55:55 +0000

    Update generated classes after commit 81ee2ba

--- src/net/JNet/Generated/Java/Beans/ExceptionListener.cs
+++ src/net/JNet/Generated/Java/Beans/ExceptionListener.cs
@@ -35,7 +35,11 @@ namespace Java.Beans
         /// Default constructor: even if the corresponding Java class does not have one, it is mandatory for JCOBridge
         /// </summary>
         public ExceptionListener() { InitializeHandlers(); }
-
+        /// <summary>
+        /// Generic constructor: it is useful for JCOBridge when there is a derived class which needs to pass arguments to the highest JVMBridgeBase class
+        /// </summary>
+        [global::System.Obsolete("ExceptionListener class represents, in .NET, an instance of a JVM interface or abstract class. This public initializer is needed for JCOBridge internal use, other uses can produce unidentible behaviors.")]
+        public ExceptionListener(params object[] args) { }
         const string _bridgeClassName = "org.mases.jnet.generated.java.beans.ExceptionListener";
         private static readonly MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType _LocalBridgeClazz = ClazzOf(_bridgeClassName);
         private static MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType LocalBridgeClazz => _LocalBridgeClazz ?? throw new global::System.InvalidOperationException($"Class {_bridgeClassName} was not found.");
