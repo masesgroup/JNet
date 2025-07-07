9646a50ce4bcdae8e21b1df9035fb779d9d3c095
MASES Public Developers Team <94312179+masesdevelopers@users.noreply.github.com>
2025-07-07 19:07:03 +0200

    Adds multiple arguments constructor on listener declaration (#647)

--- src/net/JNet/Generated/Javax/Swing/Event/TableColumnModelListener.cs
+++ src/net/JNet/Generated/Javax/Swing/Event/TableColumnModelListener.cs
@@ -35,7 +35,11 @@ namespace Javax.Swing.Event
         /// Default constructor: even if the corresponding Java class does not have one, it is mandatory for JCOBridge
         /// </summary>
         public TableColumnModelListener() { InitializeHandlers(); }
-
+        /// <summary>
+        /// Generic constructor: it is useful for JCOBridge when there is a derived class which needs to pass arguments to the highest JVMBridgeBase class
+        /// </summary>
+        [global::System.Obsolete("TableColumnModelListener class represents, in .NET, an instance of a JVM interface or abstract class. This public initializer is needed for JCOBridge internal use, other uses can produce unidentible behaviors.")]
+        public TableColumnModelListener(params object[] args) { }
         const string _bridgeClassName = "org.mases.jnet.generated.javax.swing.event.TableColumnModelListener";
         private static readonly MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType _LocalBridgeClazz = ClazzOf(_bridgeClassName);
         private static MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType LocalBridgeClazz => _LocalBridgeClazz ?? throw new global::System.InvalidOperationException($"Class {_bridgeClassName} was not found.");
