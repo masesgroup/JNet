bdd0ea7aacec15b662f939fdcbbcb8af4bd73c1e
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-09-16 03:19:46 +0200

    Alignment classes to latest available feature

--- src/net/JNet/Developed/Java/Util/Function/IntConsumer.cs
+++ src/net/JNet/Developed/Java/Util/Function/IntConsumer.cs
@@ -49,8 +49,9 @@ namespace Java.Util.Function
         protected override bool InitHandlers => false;
 
         const string _bridgeClassName = "java.util.function.IntConsumer";
-        private static readonly IJavaType _LocalBridgeClazz = ClazzOf(_bridgeClassName);
-        private static IJavaType LocalBridgeClazz => _LocalBridgeClazz ?? throw new InvalidOperationException($"Class {_bridgeClassName} was not found.");
+        private static readonly global::System.Exception _LocalBridgeClazzException = null;
+        private static readonly MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType _LocalBridgeClazz = JVMBridgeBase.ClazzOf(_bridgeClassName, out _LocalBridgeClazzException, false);
+        private static MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType LocalBridgeClazz => _LocalBridgeClazz ?? throw _LocalBridgeClazzException ?? new global::System.InvalidOperationException($"Class {_bridgeClassName} was not found.");
 
         /// <inheritdoc />
         public override string BridgeClassName => _bridgeClassName;
--- src/net/JNet/Developed/Java/Util/Function/ObjIntConsumer.cs
+++ src/net/JNet/Developed/Java/Util/Function/ObjIntConsumer.cs
@@ -55,8 +55,9 @@ namespace Java.Util.Function
         protected override bool InitHandlers => false;
 
         const string _bridgeClassName = "java.util.function.ObjIntConsumer";
-        private static readonly IJavaType _LocalBridgeClazz = ClazzOf(_bridgeClassName);
-        private static IJavaType LocalBridgeClazz => _LocalBridgeClazz ?? throw new InvalidOperationException($"Class {_bridgeClassName} was not found.");
+        private static readonly global::System.Exception _LocalBridgeClazzException = null;
+        private static readonly MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType _LocalBridgeClazz = JVMBridgeBase.ClazzOf(_bridgeClassName, out _LocalBridgeClazzException, false);
+        private static MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType LocalBridgeClazz => _LocalBridgeClazz ?? throw _LocalBridgeClazzException ?? new global::System.InvalidOperationException($"Class {_bridgeClassName} was not found.");
 
         /// <inheritdoc />
         public override string BridgeClassName => _bridgeClassName;
@@ -125,8 +126,9 @@ namespace Java.Util.Function
         protected override bool InitHandlers => false;
 
         const string _bridgeClassName = "java.util.function.ObjIntConsumer";
-        private static readonly IJavaType _LocalBridgeClazz = ClazzOf(_bridgeClassName);
-        private static IJavaType LocalBridgeClazz => _LocalBridgeClazz ?? throw new InvalidOperationException($"Class {_bridgeClassName} was not found.");
+        private static readonly global::System.Exception _LocalBridgeClazzException = null;
+        private static readonly MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType _LocalBridgeClazz = JVMBridgeBase.ClazzOf(_bridgeClassName, out _LocalBridgeClazzException, false);
+        private static MASES.JCOBridge.C2JBridge.JVMInterop.IJavaType LocalBridgeClazz => _LocalBridgeClazz ?? throw _LocalBridgeClazzException ?? new global::System.InvalidOperationException($"Class {_bridgeClassName} was not found.");
 
         /// <inheritdoc />
         public override string BridgeClassName => _bridgeClassName;
