3bb346040f773931abe22c17f2ff279b9c93947a
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-10-15 12:54:37 +0200

    https://github.com/masesgroup/JNet/issues/770#issuecomment-3405824484: commented out var arg test

--- tests/net/JNetTest/Program.cs
+++ tests/net/JNetTest/Program.cs
@@ -52,7 +52,7 @@ namespace MASES.JNetTest
             {
                 TestCreateObjects();
 
-                TestVarArg();
+                // TestVarArg(); https://github.com/masesgroup/JNet/issues/770#issuecomment-3405824484
 
                 TestEnum();
 
