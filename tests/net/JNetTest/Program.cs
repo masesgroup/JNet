c6dda73bad904ec427de9712ffbbd5123b205a7d
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2025-10-15 17:28:15 +0200

    Fix wrong cherry-pick

--- tests/net/JNetTest/Program.cs
+++ tests/net/JNetTest/Program.cs
@@ -52,6 +52,8 @@ namespace MASES.JNetTest
             {
                 TestCreateObjects();
 
+                TestVarArg();
+
                 TestEnum();
 
                 for (int i = 0; i < 5; i++)
@@ -180,6 +182,47 @@ namespace MASES.JNetTest
             }
         }
 
+        static void TestVarArg()
+        {
+            System.Console.WriteLine("TestVarArg");
+            const string formatToUSe = "This is the %d %s for a %s";
+            for (int i = 0; i < 10; i++)
+            {
+                bool fallback = false;
+                bool fallback2 = false;
+                string str;
+                var dataToUse = string.Format("This is the {0} {1} for a {2}", i, "test", "varArg");
+                try
+                {
+                    str = String.Format(formatToUSe, i, "test", "varArg");
+                }
+                catch
+                {
+                    // https://github.com/masesgroup/JNet/issues/770#issuecomment-3405824484
+                    fallback = true;
+                    try
+                    {
+                        str = String.SExecuteWithSignature("format", "(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;", formatToUSe, i, "test", "varArg").ToString();
+                    }
+                    catch
+                    {
+                        // https://github.com/masesgroup/JNet/issues/770#issuecomment-3406351861
+                        fallback2 = true;
+                        str = String.SExecute("format", formatToUSe, i, "test", "varArg").ToString();
+                    }
+                }
+
+                if (str != dataToUse)
+                {
+                    throw new System.InvalidOperationException($"Failed to compare {str} with {dataToUse}: fallback is {fallback}, fallback2 is {fallback2}");
+                }
+                else if (fallback || fallback2)
+                {
+                    System.Console.WriteLine($"Test ended in right way with fallback ({fallback}) and fallback2 ({fallback2})");
+                }
+            }
+        }
+
         static void TestEnum()
         {
             System.Console.WriteLine("TestEnum");
@@ -216,6 +259,8 @@ namespace MASES.JNetTest
             public override string BridgeClassName => "org.mases.jnet.TestListener";
 
             public TestListener() { }
+
+            public TestListener(IJVMBridgeBaseInitializer initializer) : base(initializer) { }
 
             public TestListener(params object[] args) : base(args) { }
 
@@ -449,6 +494,8 @@ namespace MASES.JNetTest
         {
             /// <inheritdoc/>
             public TestFuture() { }
+            /// <inheritdoc/>
+            public TestFuture(IJVMBridgeBaseInitializer initializer) : base(initializer) { }
 
             public override string BridgeClassName => "org.mases.jnet.TestFuture";
 
@@ -525,9 +572,12 @@ namespace MASES.JNetTest
             var newDict = map.ToNetDictiony<string, bool, Java.Lang.String, Java.Lang.Boolean>();
 
             const int execution = 10000;
+            const int numRepetition = 10;
+            System.Collections.Generic.List<System.Collections.Generic.List<long>> executionData = new System.Collections.Generic.List<System.Collections.Generic.List<long>>();
 
-            for (int index = 0; index < 10; index++)
+            for (int index = 0; index < numRepetition; index++)
             {
+                System.Collections.Generic.List<long> singleExecutionData = new System.Collections.Generic.List<long>();
                 Stopwatch w = Stopwatch.StartNew();
                 Java.Util.ArrayList<int> alist = new Java.Util.ArrayList<int>();
                 for (int i = 0; i < execution; i++)
@@ -535,6 +585,7 @@ namespace MASES.JNetTest
                     alist.Add(i);
                 }
                 w.Stop();
+                singleExecutionData.Add(w.Elapsed.Ticks);
                 System.Console.WriteLine($"ArrayList Elapsed ticks: {w.Elapsed.Ticks}");
 
                 w.Restart();
@@ -544,6 +595,7 @@ namespace MASES.JNetTest
                     nlist.Add(i);
                 }
                 w.Stop();
+                singleExecutionData.Add(w.Elapsed.Ticks);
                 var referenceValue = w.Elapsed.Ticks;
                 System.Console.WriteLine($"System.Collections.Generic.List Elapsed {w.Elapsed} - ticks: {referenceValue}");
 
@@ -553,6 +605,7 @@ namespace MASES.JNetTest
                 var tmpJList = JNetHelper.ListFrom(tmpArray);
                 alist = new Java.Util.ArrayList<int>(tmpJList);
                 w.Stop();
+                singleExecutionData.Add(w.Elapsed.Ticks);
                 System.Console.WriteLine($"Java.Util.ArrayList from array Elapsed {w.Elapsed} - ticks: {w.Elapsed.Ticks} ({100 * w.Elapsed.Ticks / referenceValue}%)");
 
                 var intBuffer = IntBuffer.From(tmpArray, false, false);
@@ -561,16 +614,32 @@ namespace MASES.JNetTest
                 tmpJList = JNetHelper.ListFrom(intBuffer);
                 alist = new Java.Util.ArrayList<int>(tmpJList);
                 w.Stop();
+                singleExecutionData.Add(w.Elapsed.Ticks);
                 System.Console.WriteLine($"Java.Util.ArrayList from array premade buffer Elapsed {w.Elapsed} - ticks: {w.Elapsed.Ticks} ({100 * w.Elapsed.Ticks / referenceValue}%)");
 
                 w.Restart();
                 tmpJList = JNetHelper.ListFrom(tmpArray, true);
                 alist = new Java.Util.ArrayList<int>(tmpJList);
                 w.Stop();
+                singleExecutionData.Add(w.Elapsed.Ticks);
                 System.Console.WriteLine($"Java.Util.ArrayList from array buffered Elapsed {w.Elapsed} - ticks: {w.Elapsed.Ticks} ({100 * w.Elapsed.Ticks / referenceValue}%)");
                 //var collection = newDict.Values.ToJCollection();
                 //var intermediate = collection.ToList<Map.Entry<string, string>>();
                 var list = ((List<int>)alist).ToList();
+
+                executionData.Add(singleExecutionData);
+            }
+
+            int numOfTests = executionData[0].Count;
+
+            for (int i = 0; i < numOfTests; i++)
+            {
+                long total = 0;
+                for (int index = 1; index < numRepetition; index++)
+                {
+                    total += executionData[index][i];
+                }
+                System.Console.WriteLine($"Test {i} Mean {System.TimeSpan.FromTicks(total / (numRepetition - 1))}");
             }
         }
     }
