c03d0e52fa85f2f69511b8d0687b94d0e91840a4
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2026-02-11 09:35:44 +0100

    Use powershell command-line to force location of JVM library

--- tests/scripts/JNetPSTest.ps1
+++ tests/scripts/JNetPSTest.ps1
@@ -1,5 +1,5 @@
 $PSVersionTable.PSVersion
-Start-JNetPS -Verbose -IgnoreUnrecognized
+Start-JNetPS -Verbose -IgnoreUnrecognized -JVMPath $args[0]
 $var = New-JObject -Class java.lang.String -Arguments "Hello from"
 $var.ToString()
 $var = $var.concat(" JNetPS")
