e698c751a66e8de0c0c240c3877a31540830bc6e
MASES Public Developers Team <94312179+masesdevelopers@users.noreply.github.com>
2026-04-16 04:52:12 +0200

    Update base class

--- src/net/JNet/Developed/Java/Lang/String.cs
+++ src/net/JNet/Developed/Java/Lang/String.cs
@@ -22,7 +22,7 @@ using SystemNet = System;
 
 namespace Java.Lang
 {
-    public partial class String : JVMBridgeCore<String>, INativeConvertible<String, string>, SystemNet.IComparable<String>, SystemNet.IEquatable<String>
+    public partial class String : JVMBridgeBase<String>, INativeConvertible<String, string>, SystemNet.IComparable<String>, SystemNet.IEquatable<String>
     {
         #region Constructors
         /// <summary>
