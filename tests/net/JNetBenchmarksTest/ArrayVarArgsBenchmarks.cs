34c8d64685b9fb463089e8b24e1e60de80277251
MASES Public Developers Team <94312179+masesdevelopers@users.noreply.github.com>
2026-09-09 03:45:37 +0200

    Add BenchmarkDotNet benchmarks and workflows (#1214)
    * Add BenchmarkDotNet benchmarks and workflows
    
    Introduce a BenchmarkDotNet-based benchmarking suite and CI workflows. Adds workflow files (benchmark-baseline.yaml, benchmark-pr.yaml, build_benchmarks.yaml + .bak) to run baseline and PR comparisons. Adds a new .NET benchmark project (JNetBenchmarksTest) with multiple benchmark classes (Invoke, Array/VarArgs, Predicate round-trip and sustained), Program and csproj. Extends Java TestPerformance with methods needed by benchmarks and updates tests solution to include the new project. Enables automated benchmark runs and PR vs baseline comparisons using dotnet/performance ResultsComparer.

---
+++ tests/net/JNetBenchmarksTest/ArrayVarArgsBenchmarks.cs
@@ -0,0 +1,69 @@
+/*
+*  Copyright (c) 2022-2026 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using MASES.JCOBridge.C2JBridge.JVMInterop;
+using MASES.JNetTest.Common;
+
+namespace MASES.JNetBenchmarksTest;
+
+[MemoryDiagnoser]
+public class ArrayVarArgsBenchmarks
+{
+    [Params(10, 1000)]
+    public int Length;
+
+    IJavaObject _instance;
+    int[] _intArray;
+    string[] _stringArray;
+    object[] _boxedIntElements;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        JNetTestCore.ApplicationWriteEventOrExceptionOnCmdLine = true;
+        JNetTestCore.ApplicationHeapSize = "4G";
+        JNetTestCore.ApplicationInitialHeapSize = "256M";
+        JNetTestCore.CreateGlobalInstance();
+
+        _instance = JNetTestCore.GlobalInstance.JVM.New("org.mases.jnet.TestPerformance") as IJavaObject;
+
+        _intArray = new int[Length];
+        _stringArray = new string[Length];
+        for (int i = 0; i < Length; i++) _stringArray[i] = "s" + i;
+
+        _boxedIntElements = new object[Length];
+        for (int i = 0; i < Length; i++) _boxedIntElements[i] = i;
+    }
+
+    [Benchmark(Baseline = true)]
+    public void InvokeIntParam() => _instance.Invoke("executeIntMethod", 42);
+
+    [Benchmark]
+    public void InvokeStringArrayFixed() => _instance.Invoke("executeStringArrayMethod", (object)_stringArray);
+
+    [Benchmark]
+    public void InvokeIntArrayFixed() => _instance.Invoke("executeIntArrayMethod", _intArray);
+
+    [Benchmark]
+    public void InvokeVarArgsWholeArray() => _instance.Invoke("executeVarArgsMethod", _intArray);
+
+    [Benchmark]
+    public void InvokeVarArgsSpreadElements() => _instance.Invoke("executeVarArgsObjectMethod", _boxedIntElements);
+}
